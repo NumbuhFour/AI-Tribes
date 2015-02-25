ea0ea1fcae9a54d171161a55050c613a7092ba7c
unknown <fad4470@IMM-30.main.ad.rit.edu>
2015-02-25 09:57:22 -0500

    Added path visualizer

---
+++ Assets/Editor/EditorPath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[CustomEditor(typeof(Path))]
+public class EditorPath : Editor {
+	
+	
+	public void OnSceneGUI () {
+		Transform debug = ((Path)this.target).transform;
+		for(int i = 0; i < debug.childCount; i++){
+			PathNode child = debug.GetChild(i).GetComponent<PathNode>();
+			child.FindNextAndLast();
+			Vector3 diff = child.Next.transform.position - child.transform.position;
+			Debug.DrawRay(child.transform.position, diff, Color.yellow,0f, false);
+
+			Handles.color = Color.blue;
+			Handles.Label(child.transform.position + new Vector3(0,0,2.3f), "["+i+"]");
+		}
+	}
+}
---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathNode : MonoBehaviour {
+	
+	private PathNode last;
+	private PathNode next;
+	
+	public PathNode Next { get { return next; } }
+	public PathNode Last { get { return last; } }
+
+	public Vector3 Position{
+		get { return this.transform.position; }
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void FindNextAndLast(){
+		Transform parent = this.transform.parent;
+		int max = parent.childCount;
+		int siblingIndex = this.transform.GetSiblingIndex();
+		next = parent.GetChild((siblingIndex + 1 >= max) ? 0:(siblingIndex+1)).GetComponent<PathNode>();
+		last = parent.GetChild((siblingIndex - 1 < 0) ? (max-1):(siblingIndex-1)).GetComponent<PathNode>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
