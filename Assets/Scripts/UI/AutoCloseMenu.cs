a8facbe7e6a47ee1c7f8c9cbc546fcaf274d7315
unknown <fad4470@GDD-17.main.ad.rit.edu>
2015-04-20 09:36:13 -0400

    Continued work on radial menu

---
+++ Assets/Scripts/UI/AutoCloseMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoCloseMenu : MonoBehaviour {
+
+	float maxDist = 120f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = ((RectTransform)this.transform).anchoredPosition;
+		float dist = (pos - (Vector2)Input.mousePosition).magnitude;
+		if(dist > maxDist){
+			//Close
+		}
+	}
+}
