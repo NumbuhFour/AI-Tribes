3cf81761089c635882be30a172ff8036ce0a06cc
unknown <fad4470@GDD-30.main.ad.rit.edu>
2015-05-06 12:52:51 -0400

    Implemented attack for predator Untested as I don't have Unity on this comp.

---
+++ Assets/Scripts/FSM/Attack.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace FSM {
+	[AddComponentMenu("Scripts/FSM/Attack")]
+	public class Attack : FSMBehaviour {
+		
+		private Species species;
+		private Movement move;
+        private PropertyTracker myProp;
+		
+		private Vector3 roamGoal;
+		private float delay = 0;
+		protected override void OnBegin(){
+			if(species == null) species = GetComponent<Species>();
+			if(move == null) move = GetComponent<Movement>();
+			delay = 0;
+			roamGoal = Roam();
+            myProp = GetComponent<PropertyTracker>();
+		}
+		
+		protected override void OnUpdate(){
+			/*if(species.IsWithinReach(roamGoal) || giveUp <= 0){
+				roamGoal = Roam();
+				wait = Random.Range(0,Random.Range(0,5));
+				giveUp = Random.Range(0,10);
+			}else if(wait > 0){
+				wait -= Time.deltaTime;
+			}else{
+				move.PathTo(roamGoal);
+				giveUp -= Time.deltaTime;
+			}*/
+            
+            if(delay <= 0){
+                GameObject target = species.targetObject;
+                PropertyTracker prop = target.GetComponent<PropertyTracker>();
+                int my = Random.range(0,myProp['strength']);
+                int opp = Random.range(0,prop['strength']);
+                
+                if(my > opp){
+                    prop['health'] -= my['strength'];
+                }
+                delay = myProp['attackSpeed'];
+
+            }else {
+                delay -= Time.deltaTime;
+            }
+		}
+		
+		public Vector3 Roam(){
+			Vector3 targetDir = Quaternion.AngleAxis(Random.Range(-45,45), this.transform.up)*this.transform.forward * Random.Range(70,200);
+			return transform.position + targetDir;
+		}
+	}
+}
--- Assets/Scripts/PropertyTracker.cs
+++ Assets/Scripts/PropertyTracker.cs
@@ -6,6 +6,11 @@ public class PropertyTracker : MonoBehaviour {
 	
 	private Dictionary<string, object> data = new Dictionary<string, object>();
 	
+    void Start(){
+        if(this['strength'] == null) this['strength'] = 0;
+        if(this['attackSpeed'] == null) this['attackSpeed'] = 1000; //Milliseconds
+    }
+    
 	[SerializeField]
 	private int maxHealth;
 	public int MaxHealth{
@@ -24,7 +29,12 @@ public class PropertyTracker : MonoBehaviour {
 			health = value;
 			if(health <= 0) {
 				this.transform.parent.BroadcastMessage("ObjectDeath", this, SendMessageOptions.DontRequireReceiver);
-				Destroy(this.gameObject);
+                other.GetComponent<Species>().enabled = false;
+                other.GetComponent<Role>().enabled = false;
+                other.GetComponent<Movement>().enabled = false;
+                foreach(Transform obj in transform)
+                    Object.Destroy(obj.gameObject);
+                other.gameObject.GetComponent<Renderer>().enabled = false;
 			}
 		}
 	}
